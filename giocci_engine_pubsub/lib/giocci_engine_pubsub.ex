a56473fcd6eb4abdcd3271050c5e688e7b75b2a4
fomfom <a>
2023-10-19 17:47:12 +0900

    create getting linux data function

--- giocci_engine_pubsub/lib/giocci_engine_pubsub.ex
+++ giocci_engine_pubsub/lib/giocci_engine_pubsub.ex
@@ -1,4 +1,6 @@
 defmodule GiocciEnginePubsub do
+
+  use GenServer, shutdown: :infinity
   @moduledoc """
   Documentation for `GiocciEnginePubsub`.
   """
@@ -17,27 +19,54 @@ defmodule GiocciEnginePubsub do
   end
 
   def start_engine_pubsub do
-    # NifZenoh.tester_sub ("demo/example/zenoh-rs-pub")
+
     session = Zenohex.open
     Session.declare_subscriber(session, "to/engine", fn m-> callback(m,session) end)
+    Session.declare_subscriber(session, "return_ftom/engine", fn m-> callbackrt(m,session) end)
 
     {:ok, publisher} = Session.declare_publisher(session, "to/faal")
     Publisher.put(publisher, "Hello zenoh?")
   end
-  def publish (session)do
-    {:ok, publisher} = Session.declare_publisher(session, "demo/example/zenoh-rs-pub")
-    Publisher.put(publisher, "Hello zenoh?")
+  def publish(session, msg)do
+    {:ok, publisher} = Session.declare_publisher(session, "to/faal")
+    Publisher.put(publisher, msg)
   end
 
-  def subscribe do
-    session = Zenohex.open
-    Session.declare_subscriber(session, "to/engine", fn m-> callback(m,session) end)
+  # def subscribe do
+  #   session = Zenohex.open
+  #   Session.declare_subscriber(session, "to/engine", fn m-> callback(m,session) end)
 
+  # end
+
+  def return_publish(session)do
+    {:ok, publisher} = Session.declare_publisher(session, "return_from/engine")
+    Publisher.put(publisher, "received")
   end
 
   defp callback(m,session) do
     IO.puts(m)
-    # publish(session)
+    return_publish(session)
   end
+
+  defp callbackrt(m,session) do
+    IO.puts(m)
+    # return_publish(session)
+  end
+  def send_linux_meminfo() do
+    session = Zenohex.open
+    [memtotal, memfree |other ]=get_linux_meminfo()
+    memtotal
+    memfree
+    publish(session,memtotal <> "," <>  memfree)
+  end
+
+  def get_linux_meminfo() do
+    File.read!("/proc/meminfo")
+    |> String.split("\n", trim: true)
+    |> List.delete(" ")
+    # |> List.keyfind(:MemFree,0)
+  end
+
+  # |> String.split(["\n"," "])
 
 end
